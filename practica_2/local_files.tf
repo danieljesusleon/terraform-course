94e5312efa86ab2117e439837db6211da0ed9840
daniel.leon <daniel.leon.ext@sodexo.com>
2023-07-04 02:36:22 -0300

    practica 2

--- practica_2/local_files.tf
+++ practica_2/local_files.tf
@@ -2,3 +2,4 @@ resource "local_file" "productos" {
     content = "Lista de productos para el mes proximo"
     filename = "productos-${random_string.sufijo[count.index].id}.txt" #dependencia explicita
 }
+
---
+++ practica_2/random.tf
@@ -0,0 +1,15 @@
+# resource "random_string" "sufijo-1" { #este es un recurso random y por si solo crea un unica cadena de caracteres random
+#     length  = 4                     #si queremos varios nombres debemos crear mas de este
+#     special = false                 #esto no es compliance y no cumple con el concepto DRY dont repeat yourself
+#     upper   = false
+#     numeric  = false
+}
+
+resource "random_string" "sufijo" { 
+    count = 5           #esta linea le indica a terraform que quiero 5 cadenas diferentes
+    length  = 4                     
+    special = false
+    upper   = false
+    numeric  = false
+}
+
