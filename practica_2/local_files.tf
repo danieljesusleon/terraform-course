84fbe1f5198b34e1ce4f9d9674c47bddf9209723
daniel.leon <daniel.leon.ext@sodexo.com>
2023-07-04 02:30:39 -0300

    practica 2

---
+++ practica_2/local_files.tf
@@ -0,0 +1,4 @@
+resource "local_file" "productos" {
+    content = "Lista de productos para el mes proximo"
+    filename = "productos-${random_string.sufijo[count.index].id}.txt" #dependencia explicita
+}
