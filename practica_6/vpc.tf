c9f5ffd46872af58ac1c88e39a5a99ca0f58c51d
Daniel Jesús León Jardine <danjard@gmail.com>
2023-07-10 19:20:53 -0300

    Update vpc.tf

--- practica_6/vpc.tf
+++ practica_6/vpc.tf
@@ -1,18 +1,25 @@
 resource "aws_vpc" "vpc_virginia" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.virginia_cidr
   tags = {
-    Name = "VPC_VIRGINIA"
-    name = "prueba"
-    env  = "Dev"
+    "Name" = "vpc_viriginia"
   }
 }
 
-resource "aws_vpc" "vpc_ohio" {
-  cidr_block = "10.20.0.0/16"
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc_virginia.id
+  cidr_block              = var.subnets[0]
+  map_public_ip_on_launch = true
   tags = {
-    Name = "VPC_OHIO"
-    name = "prueba"
-    env  = "Dev"
+    "Name" = "public_subnet"
   }
-  provider = aws.ohio
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc_virginia.id
+  cidr_block = var.subnets[1]
+  tags = {
+    "Name" = "private_subnet"
+  }
+  depends_on = [
+    aws_subnet.public_subnet
+  ]
 }
