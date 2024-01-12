aae21824b0fb9ec2fd247ae703f5ec9045738218
daniel.leon.ext- <daniel.leon.ext-@BRBRS-L05420.local>
2024-01-12 19:43:07 -0300

    practica_3

--- practica_3/bucket.tf
+++ practica_3/bucket.tf
@@ -1,17 +1,3 @@
 resource "aws_s3_bucket" "proveedores" {
-    count = 6
-    bucket = "proveedores-${random_string.sufijo[count.index].id}"
-    tags = {
-    Owner        = "Daniel"
-    Environment  = "Dev"
-    Office       = "proveedores"
-  }
-}
-
-resource "random_string" "sufijo" { 
-    count   = 6
-    length  = 8                    
-    special = false
-    upper   = false
-    numeric = false
+  bucket = "proveedores-regional-sp"
 }
