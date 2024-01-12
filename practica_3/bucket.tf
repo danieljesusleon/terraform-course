bc9fa4c3dabbd1a13327ec2aa9cf7c7df329c79d
daniel.leon.ext- <daniel.leon.ext-@AF711930.ce.sdxcorp.net>
2024-01-12 20:32:11 -0300

    fixing sintax

--- practica_3/bucket.tf
+++ practica_3/bucket.tf
@@ -1,12 +1,17 @@
 resource "aws_s3_bucket" "proveedores" {
-  count = 5
+  count  = 5
   bucket = "proveedores-${random_string.sufix[count.index].id}"
+  tags = {
+    Owner       = "DniaelLeon"
+    Environment = "Dev"
+    Office      = "test"
+  }
 }
 
 resource "random_string" "sufix" {
-  count = 5
-  length = 5
+  count   = 5
+  length  = 5
   special = false
-  upper = false
+  upper   = false
   numeric = false
 }
