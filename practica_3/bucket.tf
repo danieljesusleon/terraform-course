bb91a1f05f9a6de0845ece426a876a42f3afaec4
daniel.leon.ext- <daniel.leon.ext-@BRBRS-L05420.local>
2024-01-12 19:58:59 -0300

    practica_3

--- practica_3/bucket.tf
+++ practica_3/bucket.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "proveedores" {
-  count = 4
+  count = 5
   bucket = "proveedores-${random_string.sufix[count.index].id}"
 }
 
 resource "random_string" "sufix" {
-  count = 4
-  length = 8
+  count = 5
+  length = 5
   special = false
   upper = false
   numeric = false
