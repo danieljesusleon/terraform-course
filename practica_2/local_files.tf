04ca1670e293d89b289ecb4e91890c0275137357
daniel.leon.ext- <daniel.leon.ext-@AF711930.ce.sdxcorp.net>
2024-01-12 15:34:40 -0300

    practica_2

--- practica_2/local_files.tf
+++ practica_2/local_files.tf
@@ -1,20 +1,5 @@
-resource "local_file" "productos-1" {
+resource "local_file" "productos" {
+  count = 3
   content = "Lista de productos para el proximo mes"
-  filename = "productos-${random_string.sufijo-1.id}.txt"
-}
-resource "local_file" "productos-2" {
-  content = "Lista de productos para el proximo mes"
-  filename = "productos-${random_string.sufijo-2.id}.txt"
-}
-resource "local_file" "productos-3" {
-  content = "Lista de productos para el proximo mes"
-  filename = "productos-${random_string.sufijo-3.id}.txt"
-}
-resource "local_file" "productos-4" {
-  content = "Lista de productos para el proximo mes"
-  filename = "productos-${random_string.sufijo-4.id}.txt"
-}
-resource "local_file" "productos-5" {
-  content = "Lista de productos para el proximo mes"
-  filename = "productos-${random_string.sufijo-5.id}.txt"
+  filename = "productos-${random_string.sufijo[count.index].id}.txt"
 }
--- practica_2/random.tf
+++ practica_2/random.tf
@@ -1,44 +1,7 @@
-resource "random_string" "sufijo-1" { #este es un recurso random y por si solo crea un unica cadena de caracteres random
+resource "random_string" "sufijo" { #este es un recurso random y por si solo crea un unica cadena de caracteres random
+    count = 3
     length  = 4                     #si queremos varios nombres debemos crear mas de este
     special = false                 #esto no es compliance y no cumple con el concepto DRY dont repeat yourself
     upper   = false
     numeric  = false
 }
-
-resource "random_string" "sufijo-2" {
-    length  = 4
-    special = false
-    upper   = false
-    numeric  = false
-}
-
-resource "random_string" "sufijo-3" {
-    length  = 4
-    special = false
-    upper   = false
-    numeric  = false
-}
-
-resource "random_string" "sufijo-4" {
-    length  = 4
-    special = false
-    upper   = false
-    numeric  = false
-}
-
-resource "random_string" "sufijo-5" {
-    length  = 4
-    special = false
-    upper   = false
-    numeric  = false
-}
-
-
-# resource "random_string" "sufijo" { 
-#     count = 1           #esta linea le indica a terraform que quiero 5 cadenas diferentes
-#     length  = 4                     
-#     special = false
-#     upper   = false
-#     numeric  = false
-# }
-
